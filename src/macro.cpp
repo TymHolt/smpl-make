45787aebefffd9d1e02c6de046d2cfa5ac4c2955
Tim Holtkötter <124082719+TymHolt@users.noreply.github.com>
2026-02-20 15:37:12 +0100

    New macro class

--- src/macro.cpp
+++ src/macro.cpp
@@ -1,40 +1,22 @@
 #include <macro.hpp>
-#include <util/string_parsing.hpp>
-#include <stdexcept>
 #include <filesystem>
 #include <regex>
 
-bool EndsWith(std::string string, std::string suffix) {
-    return string.size() >= suffix.size() && 
-        string.compare(string.size() - suffix.size(), suffix.size(), suffix) == 0;
+smpl::AllRecursiveMacro::AllRecursiveMacro(std::string regex_string) {
+    m_regex_string = regex_string;
 }
 
-bool MatchPath(std::string path, std::string argument) {
-    std::regex expression(argument);
-    return std::regex_match(path, expression);
+bool MatchFilePath(std::string path, std::string regex_string) {
+    std::regex regex_expression(regex_string);
+    return std::regex_match(path, regex_expression);
 }
 
-std::string RunMacroAllRecursive(std::string argument) {
+std::string smpl::AllRecursiveMacro::Run() {
     std::string result = "";
 
-    for (const auto& entry : std::filesystem::recursive_directory_iterator(".")) {
-        if (entry.is_regular_file() && MatchPath(entry.path(), argument))
+    for (const auto& entry : std::filesystem::recursive_directory_iterator("."))
+        if (entry.is_regular_file() && MatchFilePath(entry.path(), m_regex_string))
             result += std::string(entry.path()) + std::string(" ");
-    }
 
     return result;
-}
-
-std::string Macro::RunMacro(std::string macro) {
-    /*util::StringParser string_parser;
-    string_parser.SetContent(macro);
-
-    std::string macro_name = string_parser.NextToken();
-    std::string macro_argument = string_parser.GetRemaining();
-    
-    if (macro_name == "all-recursive")
-        return RunMacroAllRecursive(macro_argument);
-    else
-        throw std::runtime_error(std::string("Unknown macro " + macro_name));*/
-    return ""; // TODO
-}
+};
--- src/macro.hpp
+++ src/macro.hpp
@@ -3,9 +3,21 @@
 
 #include <string>
 
-namespace Macro {
+namespace smpl {
     
-    std::string RunMacro(std::string macro);
+    class Macro {
+        public:
+            virtual ~Macro() = default;
+            virtual std::string Run() = 0;
+    };
+
+    class AllRecursiveMacro : public Macro {
+        private:
+            std::string m_regex_string;
+        public:
+            AllRecursiveMacro(std::string regex_string = "");
+            std::string Run() override;
+    };
 }
 
 #endif
